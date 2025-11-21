3f7738f2a508b7a35766aec02b0393e2ccc7f915
BoKyung-Hong <hbk491@hanmail.net>
2025-11-21 11:49:06 +0900

    [level 0] Title: 특수문자 출력하기, Time: 126.09 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        
+        // 문자열에서 ''나 ""를 쓰려면 \' 나 \"로 작성해야함
+    }
+}
