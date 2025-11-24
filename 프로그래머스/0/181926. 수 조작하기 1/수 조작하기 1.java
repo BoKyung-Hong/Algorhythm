b607a747491fd49f925439fdfe5046d15a8e421f
BoKyung-Hong <hbk491@hanmail.net>
2025-11-25 00:14:59 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.34 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, String control) {
+        
+        for(int i = 0; i < control.length(); i++) {  // 문자열 -> length()
+            char c = control.charAt(i);
+            
+            if(c =='w'){
+                n++;
+            } else if(c == 's'){
+                n--;
+            } else if(c == 'd') {
+                n += 10;
+            } else if(c == 'a') {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
